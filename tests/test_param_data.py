3d73d755eaca3ed46f40957d719a7052f3f6ff8b
Alex Hadley <alexhad6@gmail.com>
2023-02-16 20:52:09 -0800

    #2 Implement basic Param and Struct tests

--- tests/test_param_data.py
+++ tests/test_param_data.py
@@ -1,35 +1,48 @@
 """Tests for the paramdb._param_data module."""
 
+from datetime import datetime
 from paramdb._param_data import ParamData, Struct, Param
 
 
-def test_create_struct() -> None:
-    """Can create a parameter structure."""
-
-    class TestStruct(Struct):
-        """Test parameter structure."""
-
-    test_struct = TestStruct()
-    assert isinstance(test_struct, Struct)
-    assert isinstance(test_struct, ParamData)
+class BasicStruct(Struct):
+    """Empty parameter structure."""
 
 
-def test_create_param() -> None:
-    """Can create a parameter."""
+class BasicParam(Param):
+    """Empty parameter."""
 
-    class TestParam(Param):
-        """Test parameter."""
 
-    test_param = TestParam()
-    assert isinstance(test_param, Param)
-    assert isinstance(test_param, ParamData)
+def test_struct_isinstance() -> None:
+    """Structure object is an instance of its superclasses."""
+    basic_struct = BasicStruct()
+    assert isinstance(basic_struct, Struct)
+    assert isinstance(basic_struct, ParamData)
+
+
+def test_param_isinstance() -> None:
+    """Parameter object is an instance of its superclasses."""
+    basic_param = BasicParam()
+    assert isinstance(basic_param, Param)
+    assert isinstance(basic_param, ParamData)
 
 
 def test_struct_no_last_updated() -> None:
-    """Parameter object is an instance of `ParamData`."""
+    """Structure object that contains no parameters has no last updated time."""
+    basic_struct = BasicStruct()
+    assert basic_struct.last_updated is None
 
-    class TestStruct(Struct):
-        """Test parameter structure."""
 
-    test_struct = TestStruct()
-    assert test_struct.last_updated is None
+def test_param_init_last_updated_default() -> None:
+    """Parameter object initializes the last updated time to a default value of now."""
+    start = datetime.now()
+    basic_param = BasicParam()
+    end = datetime.now()
+    last_updated = basic_param.last_updated
+    assert start <= last_updated <= end
+
+
+def test_param_init_last_updated() -> None:
+    """Parameter object initializes the last updated time to the given value."""
+    last_updated = datetime.now()
+    basic_param = BasicParam(_last_updated=last_updated)
+    assert basic_param.last_updated == last_updated
