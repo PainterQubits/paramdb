b7f4d8cc1d2a8a5030d86b0ebb5ecf39fb141245
Alex Hadley <alexhad6@gmail.com>
2023-02-21 10:08:29 -0800

    #2 Organize tests so far

--- tests/test_param_data.py
+++ tests/test_param_data.py
@@ -5,18 +5,21 @@ from __future__ import annotations
 from time import sleep
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
-from paramdb._param_data import ParamData, Struct, Param
+import pytest
+from pytest import FixtureRequest
+from paramdb._param_data import Struct, Param
 
-NUMBER = 1.23
-STRING = "test"
+SLEEP_TIME = 0.01
+DEFAULT_NUMBER = 1.23
+DEFAULT_STRING = "test"
 
 
 @dataclass
 class CustomStruct(Struct):
     """Custom parameter structure."""
 
-    number: float = NUMBER
-    string: str = STRING
+    number: float = DEFAULT_NUMBER
+    string: str = DEFAULT_STRING
     param: CustomParam | None = None
     struct: CustomStruct | None = None
     param_list: list[CustomParam | list[CustomParam] | dict[str, CustomParam]] = field(
@@ -31,14 +34,51 @@ class CustomStruct(Struct):
 class CustomParam(Param):
     """Custom parameter."""
 
-    number: float = NUMBER
-    string: str = STRING
+    number: float = DEFAULT_NUMBER
+    string: str = DEFAULT_STRING
 
 
-def create_complex_struct() -> CustomStruct:
+def update_param_and_assert_struct_last_updated_changed(
+    param: CustomParam, struct: CustomStruct
+) -> None:
     """
-    Create a structure that contains parameters, structures, lists, and dictionaries.
+    Helper function for the following tests.
+
+    Update the given parameter (assumed to exist within the given structure) and assert
+    that the structure's last updated time correctly reflects that something was
+    just updated.
     """
+    sleep(SLEEP_TIME)  # Wait so following datetimes are different than previous ones
+    start = datetime.now()
+    param.number += 1
+    end = datetime.now()
+    assert struct.last_updated is not None and start <= struct.last_updated <= end
+
+
+@pytest.fixture(name="number")
+def fixture_number() -> float:
+    """Number used to initialize parameter data."""
+    return 1.23
+
+
+@pytest.fixture(name="string")
+def fixture_string() -> str:
+    """String used to initialize parameter data."""
+    return "test"
+
+
+@pytest.fixture(name="param_data", params=[CustomStruct, CustomParam])
+def fixture_param_data(
+    request: FixtureRequest, number: float, string: str
+) -> CustomStruct | CustomParam:
+    """Parameter data object, either a parameter or structure."""
+    param_data_class: type[CustomStruct | CustomParam] = request.param
+    return param_data_class(number=number, string=string)
+
+
+@pytest.fixture(name="complex_struct")
+def fixture_complex_struct() -> CustomStruct:
+    """Structure that contains parameters, structures, lists, and dictionaries."""
     return CustomStruct(
         param=CustomParam(),
         struct=CustomStruct(param=CustomParam()),
@@ -51,69 +91,24 @@ def create_complex_struct() -> CustomStruct:
     )
 
 
-def update_param_and_assert_struct_last_updated_changed(
-    param: CustomParam, struct: CustomStruct
+def test_property_access(
+    number: float, string: str, param_data: CustomParam | CustomStruct
 ) -> None:
-    """
-    Update the given parameter (assumed to exist within the given structure) and assert
-    that the structure's last updated time correctly reflects that something was
-    just updated.
-    """
-    sleep(0.01)  # Ensure that following times differ from previous datetime.now()'s
-    start = datetime.now()
-    param.number += 1
-    end = datetime.now()
-    assert struct.last_updated is not None and start <= struct.last_updated <= end
+    """Parameter data properties can be accessed via dot notation and index brackets."""
+    assert param_data.number == number
+    assert param_data.string == string
+    assert param_data["number"] == number
+    assert param_data["string"] == string
 
 
-def test_struct_isinstance() -> None:
-    """Structure object is an instance of its superclasses."""
-    struct = CustomStruct()
-    assert isinstance(struct, Struct)
-    assert isinstance(struct, ParamData)
-
-
-def test_param_isinstance() -> None:
-    """Parameter object is an instance of its superclasses."""
-    param = CustomParam()
-    assert isinstance(param, Param)
-    assert isinstance(param, ParamData)
-
-
-def test_struct_property_access() -> None:
-    """Structure properties can be accessed via dot notation and index brackets."""
-    struct = CustomStruct(number=NUMBER, string=STRING)
-    assert struct.number == NUMBER
-    assert struct.string == STRING
-    assert struct["number"] == NUMBER
-    assert struct["string"] == STRING
-
-
-def test_struct_property_update() -> None:
-    """Structure properties can be updated via dot notation and index brackets."""
-    struct = CustomStruct(number=NUMBER)
-    struct.number += 1
-    assert struct.number == NUMBER + 1
-    struct["number"] -= 1
-    assert struct.number == NUMBER
-
-
-def test_param_property_access() -> None:
-    """Parameter properties can be accessed via dot notation and index brakets."""
-    param = CustomParam(number=NUMBER, string=STRING)
-    assert param.number == NUMBER
-    assert param.string == STRING
-    assert param["number"] == NUMBER
-    assert param["string"] == STRING
-
-
-def test_param_property_update() -> None:
-    """Parameter properties can be updated via dot notation and index brackets."""
-    param = CustomParam(number=NUMBER)
-    param.number += 1
-    assert param.number == NUMBER + 1
-    param["number"] -= 1
-    assert param.number == NUMBER
+def test_struct_property_update(
+    number: float, param_data: CustomParam | CustomStruct
+) -> None:
+    """Parameter data properties can be updated via dot notation and index brackets."""
+    param_data.number += 1
+    assert param_data.number == number + 1
+    param_data["number"] -= 1
+    assert param_data.number == number
 
 
 def test_param_default_last_updated() -> None:
@@ -139,13 +134,16 @@ def test_param_update_last_updated() -> None:
     Parameter object updates the last updated time when a property is updated with dot
     notation or index brackets.
     """
+    # Dot notation access
     param = CustomParam()
-    sleep(0.01)  # Ensure that following times differ from previous datetime.now()'s
+    sleep(SLEEP_TIME)  # Wait so following datetimes are different than previous ones
     start = datetime.now()
     param.number += 1
     end = datetime.now()
     assert start <= param.last_updated <= end
-    sleep(0.01)  # Ensure that following times differ from previous datetime.now()'s
+
+    # Index bracket access
+    sleep(SLEEP_TIME)  # Wait so following datetimes are different than previous ones
     start = datetime.now()
     param["number"] += 1
     end = datetime.now()
@@ -158,17 +156,15 @@ def test_struct_no_last_updated() -> None:
     assert struct.last_updated is None
 
 
-def test_struct_last_updated_from_param() -> None:
+def test_struct_last_updated_from_param(complex_struct: CustomStruct) -> None:
     """Structure object can find the most recent last updated time from a parameter."""
-    complex_struct = create_complex_struct()
     param = complex_struct.param
     assert param is not None
     update_param_and_assert_struct_last_updated_changed(param, complex_struct)
 
 
-def test_struct_last_updated_from_struct() -> None:
+def test_struct_last_updated_from_struct(complex_struct: CustomStruct) -> None:
     """Structure object can find the most recent last updated time from a structure."""
-    complex_struct = create_complex_struct()
     struct = complex_struct.struct
     assert struct is not None
     param_in_struct = struct.param
@@ -176,23 +172,21 @@ def test_struct_last_updated_from_struct() -> None:
     update_param_and_assert_struct_last_updated_changed(param_in_struct, complex_struct)
 
 
-def test_struct_last_updated_from_list() -> None:
+def test_struct_last_updated_from_param_in_list(complex_struct: CustomStruct) -> None:
     """
     Structure object can find the most recent last updated time from a parameter within
     a list.
     """
-    complex_struct = create_complex_struct()
     param_in_list = complex_struct.param_list[0]
     assert isinstance(param_in_list, CustomParam)
     update_param_and_assert_struct_last_updated_changed(param_in_list, complex_struct)
 
 
-def test_struct_last_updated_from_list_in_list() -> None:
+def test_struct_last_updated_from_list_in_list(complex_struct: CustomStruct) -> None:
     """
     Structure object can find the most recent last updated time from a parameter in a
     list within a list.
     """
-    complex_struct = create_complex_struct()
     list_in_list = complex_struct.param_list[1]
     assert isinstance(list_in_list, list)
     param_in_list_in_list = list_in_list[0]
@@ -201,12 +195,11 @@ def test_struct_last_updated_from_list_in_list() -> None:
     )
 
 
-def test_struct_last_updated_from_dict_in_list() -> None:
+def test_struct_last_updated_from_dict_in_list(complex_struct: CustomStruct) -> None:
     """
     Structure object can find the most recent last updated time from a parameter in a
     dictionary within a list.
     """
-    complex_struct = create_complex_struct()
     dict_in_list = complex_struct.param_list[2]
     assert isinstance(dict_in_list, dict)
     param_in_dict_in_list = dict_in_list["p1"]
@@ -215,23 +208,21 @@ def test_struct_last_updated_from_dict_in_list() -> None:
     )
 
 
-def test_struct_last_updated_from_dict() -> None:
+def test_struct_last_updated_from_param_in_dict(complex_struct: CustomStruct) -> None:
     """
     Structure object can find the most recent last updated time from a parameter within
     a dictionary.
     """
-    complex_struct = create_complex_struct()
     param_in_dict = complex_struct.param_dict["param"]
     assert isinstance(param_in_dict, CustomParam)
     update_param_and_assert_struct_last_updated_changed(param_in_dict, complex_struct)
 
 
-def test_struct_last_updated_from_list_in_dict() -> None:
+def test_struct_last_updated_from_list_in_dict(complex_struct: CustomStruct) -> None:
     """
     Structure object can find the most recent last updated time from a parameter in a
     list within a list.
     """
-    complex_struct = create_complex_struct()
     list_in_dict = complex_struct.param_dict["list"]
     assert isinstance(list_in_dict, list)
     param_in_list_in_list = list_in_dict[0]
@@ -240,12 +231,11 @@ def test_struct_last_updated_from_list_in_dict() -> None:
     )
 
 
-def test_struct_last_updated_from_dict_in_dict() -> None:
+def test_struct_last_updated_from_dict_in_dict(complex_struct: CustomStruct) -> None:
     """
     Structure object can find the most recent last updated time from a parameter in a
     dictionary within a list.
     """
-    complex_struct = create_complex_struct()
     dict_in_dict = complex_struct.param_dict["dict"]
     assert isinstance(dict_in_dict, dict)
     param_in_dict_in_list = dict_in_dict["p1"]
