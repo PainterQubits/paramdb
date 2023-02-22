b9dafd8eb8c551275c63c30d8da03df496697e4f
Alex Hadley <alexhad6@gmail.com>
2023-02-22 11:36:43 -0800

    #4 Create initial CI workflow

--- tests/helpers.py
+++ tests/helpers.py
@@ -18,17 +18,17 @@ def sleep_for_datetime() -> None:
     time.sleep(0.001)  # Wait for one millisecond
 
 
-def update_param_and_assert_struct_last_updated_changed(
-    param: CustomParam, struct: CustomStruct
+def update_param_and_assert_last_updated_changed(
+    param: CustomParam, param_data: CustomStruct | CustomParam
 ) -> None:
     """
-    Update the given parameter (assumed to exist within the given structure) and assert
-    that the structure's last updated time correctly reflects that something was
-    just updated.
+    Update the given parameter (assumed to be or exist within the given parameter data)
+    and assert that the structure's last updated time correctly reflects that something
+    was just updated.
     """
     start = datetime.now()
     sleep_for_datetime()
     param.number += 1
     sleep_for_datetime()
     end = datetime.now()
-    assert struct.last_updated is not None and start < struct.last_updated < end
+    assert param_data.last_updated is not None and start < param_data.last_updated < end
