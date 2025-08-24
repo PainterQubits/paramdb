fac5ac6daca09117892a72d076ecda91d542a10a
Alex Hadley <alexhad6@users.noreply.github.com>
2025-08-24 13:26:36 -0700

    Fix flake8

--- tests/test_database.py
+++ tests/test_database.py
@@ -391,7 +391,8 @@ def test_commit_load_new_dataclass_field(db_path: str) -> None:
     param_db = ParamDB[CustomParam](db_path)
     param_db.commit("Initial commit", custom_param)
 
-    class CustomParam(ParamDataclass):  # type: ignore[no-redef] # pylint: disable=function-redefined
+    # pylint: disable-next=function-redefined
+    class CustomParam(ParamDataclass):  # type: ignore[no-redef]
         """Parameter dataclass with an added field."""
 
         number1: int
